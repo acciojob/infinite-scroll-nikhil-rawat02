8ce961e8ecd1319ffa882b422375fc30f35a6ead
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-07-10 11:25:11 +0530

    solution

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
 
+const infiList = document.getElementById('infi-list');
+
+// Populate initial list items lets take dummy list
+for (let i = 1; i <= 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.innerText = `List Item ${i}`;
+  infiList.appendChild(listItem);
+}
+function isEndOfList() { 
+    return infiList.scrollHeight - infiList.scrollTop === infiList.clientHeight;
+}
+
+function addMoreItems() {
+  for (let i = 1; i <= 2; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = `List Item ${infiList.children.length + i}`;
+    infiList.appendChild(listItem);
+  }
+}
+
+infiList.addEventListener('scroll', () => {
+  if (isEndOfList()) {
+    addMoreItems();
+  }
+});
